eb537aada47c22002ac223c32618c4d0e829cf97
Ori Kam <orik2005@gmail.com>
2024-04-27 23:50:36 +0300

    add circle
    Signed-off-by: Ori Kam <orik2005@gmail.com>

--- tests.py
+++ tests.py
@@ -12,26 +12,54 @@ def show_image_from_data_gray(data):
     s = Image.fromarray(data, 'L')
     s.show()
 
-filename = 'img\screen_shot_15.png'
+
+def create_circular_mask(h, w, center=None, radius=None):
+
+    if center is None: # use the middle of the image
+        center = (int(w/2), int(h/2))
+    if radius is None: # use the smallest distance between the center and image walls
+        radius = min(center[0], center[1], w-center[0], h-center[1])
+
+    Y, X = np.ogrid[:h, :w]
+    dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
+
+    mask = dist_from_center <= radius
+    print(mask.shape)
+    return mask
+
+
+
+filename = 'img\screen_shot_33.png'
 with Image.open(filename) as img:
+    
     img.load()
     pos = (0,0)
     for i in range(1):
         start = perf_counter()
         gray_img = img.convert("L") 
         data = np.array(img)
+        print(data.shape)
+        circle = create_circular_mask(len(data),len(data))
+        show_image_from_data_gray(circle * 255)
         dataImage = cv2.cvtColor(data, cv2.COLOR_BGR2RGB)
         gray_data = np.array(gray_img)
+        show_image_from_data_gray(gray_data)
+        show_image_from_data_gray(gray_data * circle)
+        print(gray_data.shape)
+
+        ones = np.ones_like(gray_data) * 255
+
+        show_image_from_data_gray(ones)
         #open_cv_image = data[:, :, ::-1].copy() 
         #grayImage = cv2.cvtColor(data, cv2.COLOR_RGBA2GRAY)
         #data = np.array(gray_img) 
         #print(len(data))
         #data_th = np.where(gray_data < 10, 255, 0)
         #print(f'{type(data_th)}, {type(gray_data)}')
-        ret,thresh2 = cv2.threshold(gray_data,10,255,cv2.THRESH_BINARY_INV)
+        ret,thresh2 = cv2.threshold(gray_data * circle,10,255,cv2.THRESH_BINARY_INV)
         #show_image_from_data_gray(thresh2)
 
-        output = cv2.connectedComponentsWithStats(thresh2, 4, cv2.CV_32S)
+        output = cv2.connectedComponentsWithStats(thresh2 * circle, 4, cv2.CV_32S)
         (numLabels, labels, stats, centroids) = output
         for s in stats:
             print(s)
@@ -80,6 +108,10 @@ with Image.open(filename) as img:
 #data = np.array(img) #0.0001846999512054026
 #running on 317 *317 = 0.0789311999687925
 #cv2.imshow("Output", thresh2)
+# construct a mask for the current connected component by
+# finding a pixels in the labels array that have the current
+# connected component ID
+#componentMask = (labels == i).astype("uint8") * 255
 def create_image(name, data):
     name.putdata(data.flatten())
     name.show()
